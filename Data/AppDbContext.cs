df9f48afd4c16cd15a241f591bf8d5b3710c5ee4
TUF-Mani <manivakili907@gmail.com>
2026-06-15 10:14:07 +0330

    Changed db path to SpecialFolder

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
 using ProjectManagementSystem.Models;
 
 namespace ProjectManagementSystem.Data;
@@ -8,6 +10,13 @@ public class AppDbContext : DbContext {
     public DbSet<User> Users => Set<User>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-        optionsBuilder.UseSqlite("Data Source=project_management.db");
+        var dbPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "ProjectManagementSystem",
+            "project_management.db"
+            );
+
+        Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
+        optionsBuilder.UseSqlite($"Data Source={dbPath}");
     }
 }
