0136560f685c6b1e3d15fb4168564efb09d34a0d
Mani <manivakili907@email.com>
2026-06-20 00:52:17 +0330

    Accent color now applies to user cards in welcome page

---
+++ Converters/AccentColorNameToPrimaryConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Avalonia.Data.Converters;
+using ProjectManagementSystem.Models;
+
+namespace ProjectManagementSystem.Converters;
+
+public class AccentColorNameToPrimaryConverter : IValueConverter {
+    public static readonly AccentColorNameToPrimaryConverter Instance = new();
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        if (value is not string name) return null;
+        return AccentColorsBase.AccentColors
+                   .FirstOrDefault(c => c.Name == name)?.PrimaryColor.Color
+               ?? AccentColorsBase.AccentColors.First().PrimaryColor.Color;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
---
+++ Converters/AccentColorToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Avalonia.Data.Converters;
+using ProjectManagementSystem.Models;
+
+namespace ProjectManagementSystem.Converters;
+
+public class AccentColorNameToBrushConverter : IValueConverter {
+    
+    public static readonly AccentColorNameToBrushConverter Instance = new();
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        if (value is not string name) return null;
+        return AccentColorsBase.AccentColors
+                   .FirstOrDefault(c => c.Name == name)?.PrimaryColor
+               ?? AccentColorsBase.AccentColors.First().PrimaryColor;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
