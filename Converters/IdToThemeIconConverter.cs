42d3e62d90105facd63e87a9e1492d8b73538db0
Mani <manivakili907@email.com>
2026-06-20 11:04:57 +0330

    Updated welcome page screenshot

---
+++ Converters/IdToThemeIconConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace ProjectManagementSystem.Converters;
+
+public class ThemeToIconConverter : IValueConverter {
+    
+    public static readonly ThemeToIconConverter Instance = new();
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        if (value is string theme) {
+            
+        }
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
